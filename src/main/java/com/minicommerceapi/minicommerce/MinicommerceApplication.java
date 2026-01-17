9e0c2d353e2d51b6c340d7f2fca65221635722f4
Denis Kaynar <kaynar.denis@gmail.com>
2026-01-17 17:22:42 +0300

    first run

--- src/main/java/com/minicommerceapi/minicommerce/MinicommerceApplication.java
+++ src/main/java/com/minicommerceapi/minicommerce/MinicommerceApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MinicommerceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MinicommerceApplication.class, args);
 	}
-
 }
